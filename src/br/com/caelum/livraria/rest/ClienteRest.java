19b6ece24adea19ee035d7234596d0c9f78b789d
jpfaria <jpfaria@gmail.com>
2014-02-12 21:01:16 -0200

    .

--- src/br/com/caelum/livraria/rest/ClienteRest.java
+++ src/br/com/caelum/livraria/rest/ClienteRest.java
@@ -2,25 +2,45 @@ package br.com.caelum.livraria.rest;
 
 import java.io.Serializable;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+
 import org.springframework.stereotype.Component;
 
+import br.com.caelum.livraria.modelo.Link;
 import br.com.caelum.livraria.modelo.Pagamento;
 import br.com.caelum.livraria.modelo.Transacao;
 
 @Component
 public class ClienteRest implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
-	private static final String SERVER_URI = "http://fj36webservicerest.herokuapp.com";
-	private static final String ENTRY_POINT = "/v1/pagamento/";
+	private static final String SERVER_URI = "http://localhost:8080/fj36-webservice";
+	private static final String ENTRY_POINT = "/pagamento/";
 
 	public Pagamento criarPagamento(Transacao transacao) {
-		return null;
+		Client cliente = ClientBuilder.newClient();
+		Pagamento resposta = cliente.target(SERVER_URI + ENTRY_POINT).request()
+				.buildPost(Entity.json(transacao)).invoke(Pagamento.class);
+		System.out.println("Pagamento criado, id:" + resposta.getId());
+
+		return resposta;
 	}
 
 	public Pagamento confirmarPagamento(Pagamento pagamento) {
-		return null;
+
+		Link linkConfirmar = pagamento.getLinkPeloRel("confirmar");
+		Client cliente = ClientBuilder.newClient();
+
+		Pagamento resposta = cliente
+				.target(SERVER_URI + linkConfirmar.getUri()).request()
+				.build(linkConfirmar.getMethod()).invoke(Pagamento.class);
+		
+		System.out.println("Pagamento criado, id:" + resposta.getId());
+
+		return resposta;
 	}
 
 }
---
+++ src/br/com/caelum/livraria/rest/TestaOAuth2.java
@@ -0,0 +1,46 @@
+package br.com.caelum.livraria.rest;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.oltu.oauth2.client.OAuthClient;
+import org.apache.oltu.oauth2.client.URLConnectionClient;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.response.OAuthAccessTokenResponse;
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+
+public class TestaOAuth2 {
+
+	public static void main(String[] args) throws OAuthSystemException, OAuthProblemException, URISyntaxException {
+
+		String tokenEndPoint = "http://fj36webservicerest-oauthserver.herokuapp.com/oauth/token";
+
+		OAuthClientRequest request = OAuthClientRequest
+				.tokenLocation(tokenEndPoint).setGrantType(GrantType.PASSWORD)
+				.setClientId("oauth2_client_id")
+				.setClientSecret("oauth2_client_secret")
+				.setUsername("fake_user").setPassword("passwd")
+				.buildBodyMessage();
+		
+		OAuthClient oAuthClient  = new OAuthClient(new URLConnectionClient());
+		OAuthAccessTokenResponse oAuthResponse = oAuthClient.accessToken(request);
+		
+		System.out.println("Access token: " + oAuthResponse.getAccessToken());
+		
+		Client client = ClientBuilder.newClient();
+		
+		WebTarget target = client.target(new URI("http://fj36webservicerest-seguro.herokuapp.com/v1/pagamento/1"));
+		
+		String entity = target.request(MediaType.APPLICATION_XML_TYPE).header("Authorization", "Bearer " + oAuthResponse.getAccessToken()).get(String.class);
+		
+		System.out.println("Resposta: " + entity);
+
+	}
+}
